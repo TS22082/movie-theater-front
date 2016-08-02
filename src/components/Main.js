f8f72bf76c0cf038da992634543fe7982d9b5c0d
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-08-02 16:44:19 -0700

    wip

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,20 +5,19 @@ GoogleMapsLoader.KEY = 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
 
 class Main extends Component {
   componentDidMount() {
-    const element = document.querySelector( '.map' )
+    const element = document.querySelector( '.main' )
     const options = {
-      center: { lat: 37.7918786, lng: -122.3688786 },
-      scrollwheel: false,
+      center: { lat: 37.804444, lng: -122.270833 },
+      scaleControl: true,
       zoom: 10
     }
 
     GoogleMapsLoader.load( google => new google.maps.Map( element, options ))
   }
-
   render() {
     return (
       <div className="main">
-        <div className="map"></div>
+
       </div>
     )
   }
